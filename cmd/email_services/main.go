3d3e286d58473224ab80741a89891d4c2a230460
0mynk <myadav@ancilar.com>
2025-05-21 17:07:44 +0530

    Implement email sending logic for user signup events

--- cmd/email_services/main.go
+++ cmd/email_services/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"time"
 
 	"user-signup-rabbitmq/pkg/common"
 
 	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 )
 
 func init() {
@@ -19,7 +23,42 @@ func init() {
 
 func processEmail(event common.UserEvent) {
 	// Get SMTP configuration
-	// TODO: Implement email sending logic
+	smtpHost := os.Getenv("SMTP_HOST")
+	smtpPort, _ := strconv.Atoi(os.Getenv("SMTP_PORT"))
+
+	if smtpHost == "" || smtpPort == 0 {
+		log.Println("SMTP configuration missing, skipping email send")
+		return
+	}
+
+	// Create email message
+	m := gomail.NewMessage()
+	m.SetHeader("From", "ancilar@gmail.com")
+	m.SetHeader("To", event.Email)
+	m.SetHeader("Subject", "Welcome to Our Service!")
+
+	htmlBody := fmt.Sprintf(`
+		<html>
+		<body>
+			<h1>Welcome, %s %s!</h1>
+			<p>Thank you for signing up!</p>
+			<p>We're excited to have you on board.</p>
+		</body>
+		</html>
+	`, event.FirstName, event.LastName)
+
+	m.SetBody("text/html", htmlBody)
+
+	// Send email using MailHog
+	d := gomail.NewDialer(smtpHost, smtpPort, "", "")
+	// d.Timeout = 10 * time.Second
+
+	if err := d.DialAndSend(m); err != nil {
+		log.Printf("Failed to send email to %s: %v", event.Email, err)
+		return
+	}
+
+	log.Printf("Email sent to %s", event.Email)
 	time.Sleep(1 * time.Second)
 }
 
